a9e79528d1eb2a64305d93c22c7f32d177da3723
gd47rajan <gd47rajan@GD111>
2017-06-13 20:18:48 -0700

    beep boop

---
+++ Assignment 3/Assignment 3/Dragon's Blood.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class DragonsBlood : Plant
+    {
+        public DragonsBlood()
+        {
+            {
+                Cost = 500f;
+                SellRatio = 75f;
+            }
+        }
+    } 
+ 
+}
