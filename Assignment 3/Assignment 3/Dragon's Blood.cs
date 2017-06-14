adc5fa6a198a8d575f239a903d3fa01fa979166d
gd47rajan <gd47rajan@GD111>
2017-06-13 20:42:29 -0700

    Comments on the dragon's blood class. Can't forget those comments.

--- Assignment 3/Assignment 3/Dragon's Blood.cs
+++ Assignment 3/Assignment 3/Dragon's Blood.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 namespace Assignment_3
 {
     class DragonsBlood : Plant
-    {
+    {   /* While Dragon's Blood is not typically grow in large scale crops in our game it does
+        in our game. It is the beief that matters. The following lines of code are for the new 
+        child class for the Dragon's Blood Tree
+        */
+        
         public DragonsBlood()
         {
             {
                 Cost = 500f;
                 SellRatio = 75f;
             }
-        }
+ 
+        } 
+
     } 
  
 }
