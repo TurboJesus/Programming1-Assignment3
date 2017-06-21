8dd25ae1f9d3ee7d285ae0704b6f644ba6efa4fb
gd47rajan <gd47rajan@GD111>
2017-06-21 12:54:51 -0700

    Lines 24-47, welcoming the player to the game. As well as creating the object.

--- Assignment 3/Assignment 3/Dragon's Blood.cs
+++ Assignment 3/Assignment 3/Dragon's Blood.cs
@@ -17,7 +17,6 @@ namespace Assignment_3
             {
                 Cost = 500f;
                 SellRatio = 75f;
-
             }
  
         } 
--- Assignment 3/Assignment 3/Program.cs
+++ Assignment 3/Assignment 3/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
     Assignment 3. It should take in several forms of input from the player, 
     things like: what to plant, what to water, what to feed, what to harvest.
     Not 100% sure what the interface will look like, but we'll try stuff out.
-    It was made by Ali, Dayton Ruhl, Rajan.
+    It was made by Ali, Dayton Ruhl, & Rajan.
  */
 
 namespace Assignment_3
@@ -20,9 +20,31 @@ namespace Assignment_3
         static void Main(string[] args)
         {
             iMoney = 100;
-            Console.WriteLine("Hey hey wadda ya say?");
 
+            /*Welcoming the player to game & letting them know what they
+            can expect from Gardening Simulator 2017. Lines 36-40 are for 
+            creating the name of the garden. Line 43 is for creating the 
+            object for this program, it is an object oriented program after 
+            all.
+            */
 
+            Console.WriteLine("Gardening Simulator 2017");
+            Console.WriteLine("Brought to you by Ali, Dayton, & Rajan");
+            Console.ReadLine();
+
+            Console.WriteLine("You have {0} to start with", iMoney);
+            Console.WriteLine("In Gardening Simulator 2017, you can buy plants, grow them, sell them, and more!");
+            Console.ReadLine();
+
+            Console.WriteLine("What would you like to name your garden?");
+            string sGardenName = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine("WOW, {0}, what a great name!", sGardenName);
+            Console.ReadLine();
+
+            Plant myPlant = new Plant();
+
+    
         }
     }
 }
